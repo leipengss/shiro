8c19d06b3a2c14f3caf299e146473a26ff8ab330
leipengss <657182928@qq.com>
2020-03-25 19:00:35 +0800

    长长的

--- Shiro-SpringBoot-master/shiroSimple/src/main/java/com/howie/shiro/model/ResultMap.java
+++ Shiro-SpringBoot-master/shiroSimple/src/main/java/com/howie/shiro/model/ResultMap.java
@@ -17,18 +17,22 @@ public class ResultMap extends HashMap<String, Object> {
     public ResultMap() {
     }
 
+    
     public ResultMap success() {
         this.put("result", "success");
         return this;
     }
 
+    
     public ResultMap fail() {
         this.put("result", "fail");
         return this;
     }
 
+    
     public ResultMap message(Object message) {
         this.put("message", message);
+        System.out.println("ttttttttttttttttttttttttttt");
         return this;
     }
 }
